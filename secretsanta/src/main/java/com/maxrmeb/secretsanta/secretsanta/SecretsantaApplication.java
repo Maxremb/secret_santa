4e88db99ddd834493ecd5ea9dd68b26789ba4cd0
Maxremb <61730745+Maxremb@users.noreply.github.com>
2025-10-20 06:32:32 +0000

    ajout RAF

--- secretsanta/src/main/java/com/maxrmeb/secretsanta/secretsanta/SecretsantaApplication.java
+++ secretsanta/src/main/java/com/maxrmeb/secretsanta/secretsanta/SecretsantaApplication.java
@@ -9,6 +9,14 @@ public class SecretsantaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecretsantaApplication.class, args);
+
+		/*
+		 * RAF  :
+		 * Crée modeèle personne (nom/prénom/email,telephone ?)
+		 * 
+		 * Rajouter dépendance spring mail et implémenter solution envoie email
+		 * Recherche si méthode similaire par téléphone (SMS)
+		 */
 	}
 
 }
